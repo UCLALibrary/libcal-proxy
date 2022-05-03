056cc288d54e9620723765b173c15d817377255d
David Rickard <drickard1967@library.ucla.edu>
2022-05-03 23:34:13 +0000

    code cleanup, style and PMD fixes

--- src/main/java/edu/ucla/library/libcal/utils/TokenUtils.java
+++ src/main/java/edu/ucla/library/libcal/utils/TokenUtils.java
@@ -1,3 +1,4 @@
+
 package edu.ucla.library.libcal.utils;
 
 import edu.ucla.library.libcal.JsonKeys;
@@ -10,16 +11,16 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
 import io.vertx.ext.auth.oauth2.OAuth2Options;
 
 /**
  * Utility method for retrieving OAuth access tokens.
-*/
-public class TokenUtils {
+ */
+public final class TokenUtils {
 
+    /** The logger used by the token utilities. */
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenUtils.class, MessageCodes.BUNDLE);
 
     /**
@@ -29,51 +30,49 @@ public class TokenUtils {
     }
 
     /**
-      * Retrieves an access token from LibCal OAuth provider.
-      *
-      * @param aClientInfo JSON object holding client ID and secret
-      * @param aVertx A Vertx object used to build token request
-      * @return JSON object wrapping the access token
-    */
+     * Retrieves the bearer token from LibCal OAuth provider response.
+     *
+     * @param aClientInfo JSON object holding client ID and secret
+     * @param aVertx A Vertx object used to build token request
+     * @return JSON object wrapping the access token
+     */
     public static Future<JsonObject> getAccessToken(final JsonObject aClientInfo, final Vertx aVertx) {
-      return handleRawToken(aClientInfo, aVertx).compose(rawToken -> {
-          //final String accessToken = rawToken.getString(JsonKeys.ACCESS_TOKEN);
-	  final JsonObject accessToken = new JsonObject();
-          try {
-	      accessToken.put(JsonKeys.ACCESS_TOKEN, rawToken.getString(JsonKeys.ACCESS_TOKEN));
-	      LOGGER.info(MessageCodes.LCP_002, accessToken.encodePrettily());
-
-	  } catch (Exception details) {
-	      accessToken.put(JsonKeys.TOKEN_ERROR, rawToken.toString());
-	  }
-	      System.out.println(accessToken.encodePrettily());
-          return Future.succeededFuture(accessToken);
-      });
-
+        return handleRawToken(aClientInfo, aVertx).compose(rawToken -> {
+            final JsonObject accessToken = new JsonObject();
+            LOGGER.info(MessageCodes.LCP_002, accessToken.encodePrettily());
+	    if (rawToken.containsKey(JsonKeys.ACCESS_TOKEN)) {
+                accessToken.put(JsonKeys.ACCESS_TOKEN, rawToken.getString(JsonKeys.ACCESS_TOKEN));
+	    } else {
+                accessToken.put(JsonKeys.TOKEN_ERROR, rawToken.toString());
+	    }
+            return Future.succeededFuture(accessToken);
+        });
     }
 
+    /**
+     * Retrieves an access token from LibCal OAuth provider.
+     *
+     * @param aClientInfo JSON object holding client ID and secret
+     * @param aVertx A Vertx object used to build token request
+     * @return JSON object wrapping the access token
+     */
     private static Future<JsonObject> handleRawToken(final JsonObject aClientInfo, final Vertx aVertx) {
-      final Promise<JsonObject> promise = Promise.promise();
+        final Promise<JsonObject> promise = Promise.promise();
 
-      OAuth2Options credentials = new OAuth2Options()
-        .setFlow(OAuth2FlowType.CLIENT)
-        .setClientId(aClientInfo.getString(JsonKeys.CLIENT_ID))
-        .setClientSecret(aClientInfo.getString(JsonKeys.CLIENT_SECRET))
-        .setSite(aClientInfo.getString(JsonKeys.TOKEN_ENDPOINT));
+        final OAuth2Options credentials = new OAuth2Options().setFlow(OAuth2FlowType.CLIENT)
+                .setClientId(aClientInfo.getString(JsonKeys.CLIENT_ID))
+                .setClientSecret(aClientInfo.getString(JsonKeys.CLIENT_SECRET))
+                .setSite(aClientInfo.getString(JsonKeys.TOKEN_ENDPOINT));
 
-      OAuth2Auth oauth2 = OAuth2Auth.create(aVertx, credentials);
+        final OAuth2Auth oauth2 = OAuth2Auth.create(aVertx, credentials);
 
-      JsonObject tokenConfig = new JsonObject();
-      oauth2.authenticate(tokenConfig)
-        .onSuccess(user -> {
-          LOGGER.info("principal : " + user.principal().encodePrettily());
-          promise.complete(user.principal());
+        final JsonObject tokenConfig = new JsonObject();
+        oauth2.authenticate(tokenConfig).onSuccess(user -> {
+            promise.complete(user.principal());
         }).onFailure(err -> {
-          promise.fail(err.getMessage());
-	      System.out.println(err.getMessage());
-	  LOGGER.error(MessageCodes.LCP_003, err.getMessage());
-	  err.printStackTrace();
-      });
-      return promise.future();
+            LOGGER.error(MessageCodes.LCP_003, err.getMessage());
+            promise.fail(err.getMessage());
+        });
+        return promise.future();
     }
 }
--- src/test/java/edu/ucla/library/libcal/utils/TokenUtilsTest.java
+++ src/test/java/edu/ucla/library/libcal/utils/TokenUtilsTest.java
@@ -1,6 +1,6 @@
+
 package edu.ucla.library.libcal.utils;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
 
@@ -11,34 +11,40 @@ import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.auth.User;
 
 import edu.ucla.library.libcal.Config;
 import edu.ucla.library.libcal.JsonKeys;
 
+/**
+ * Tests related to the TokenUtils class.
+ */
 public class TokenUtilsTest {
-    
+
+    /**
+     * Tests the <code>getAccessToken()</code> method.
+     */
     @Test
-    public final void testGetToken() {
+    public final void testGetAccessToken() {
         final Vertx vertx = Vertx.vertx();
         final ConfigStoreOptions envPropsStore = new ConfigStoreOptions().setType("env");
         final ConfigStoreOptions sysPropsStore = new ConfigStoreOptions().setType("sys");
-        final ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(envPropsStore).addStore(sysPropsStore);
+        final ConfigRetrieverOptions options =
+                new ConfigRetrieverOptions().addStore(envPropsStore).addStore(sysPropsStore);
         final ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
 
         retriever.getConfig(ar -> {
-           if (ar.failed()) {
-             System.err.println(ar.cause().getMessage());
-           } else {
-             final JsonObject config = ar.result();
-             final JsonObject clientInfo = new JsonObject()
-                   .put(JsonKeys.CLIENT_ID, config.getString(Config.OAUTH_CLIENT_ID))
-                   .put(JsonKeys.CLIENT_SECRET, config.getString(Config.OAUTH_CLIENT_SECRET))
-                   .put(JsonKeys.TOKEN_ENDPOINT, config.getString(Config.OAUTH_TOKEN_URL));
-             final JsonObject accessToken = TokenUtils.getAccessToken(clientInfo,vertx).result();
-             assertNotNull(accessToken);
-	     assertTrue(accessToken.containsKey(JsonKeys.ACCESS_TOKEN));
-           }      
+            if (ar.failed()) {
+                System.err.println(ar.cause().getMessage());
+            } else {
+                final JsonObject config = ar.result();
+                final JsonObject clientInfo =
+                        new JsonObject().put(JsonKeys.CLIENT_ID, config.getString(Config.OAUTH_CLIENT_ID))
+                                .put(JsonKeys.CLIENT_SECRET, config.getString(Config.OAUTH_CLIENT_SECRET))
+                                .put(JsonKeys.TOKEN_ENDPOINT, config.getString(Config.OAUTH_TOKEN_URL));
+                final JsonObject accessToken = TokenUtils.getAccessToken(clientInfo, vertx).result();
+                assertNotNull(accessToken);
+                assertTrue(accessToken.containsKey(JsonKeys.ACCESS_TOKEN));
+            }
         });
     }
 }
