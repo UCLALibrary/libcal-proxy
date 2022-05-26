b2f1d33e47e1c78813482fbd844c7316ec971add
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2022-05-26 11:20:24 -0700

    Implement authentication retry

--- src/main/java/edu/ucla/library/libcal/services/OAuthTokenServiceImpl.java
+++ src/main/java/edu/ucla/library/libcal/services/OAuthTokenServiceImpl.java
@@ -1,6 +1,8 @@
 
 package edu.ucla.library.libcal.services;
 
+import java.util.Optional;
+
 import edu.ucla.library.libcal.Config;
 import edu.ucla.library.libcal.Constants;
 import edu.ucla.library.libcal.JsonKeys;
@@ -9,7 +11,10 @@ import edu.ucla.library.libcal.MessageCodes;
 import info.freelibrary.util.Logger;
 import info.freelibrary.util.LoggerFactory;
 
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.auth.User;
@@ -95,15 +100,49 @@ public class OAuthTokenServiceImpl implements OAuthTokenService {
      * @return The ID of a Vert.x timer
      */
     private long keepTokenFresh(final User aToken, final int aSecondsBeforeExpiration) {
-        final int tokenExpiresIn = aToken.principal().getInteger(JsonKeys.EXPIRES_IN);
+        final int delay = aToken.principal().getInteger(JsonKeys.EXPIRES_IN) - aSecondsBeforeExpiration;
 
-        return myVertx.setTimer(tokenExpiresIn - aSecondsBeforeExpiration, timerID -> {
-            // FIXME: retry on failure; see https://vertx.io/docs/vertx-auth-oauth2/java/#_refresh_token
-            myAuthProvider.authenticate(new JsonObject()).compose(newToken -> {
-                myTimerId = keepTokenFresh(newToken, aSecondsBeforeExpiration);
+        return myVertx.setTimer(delay, timerID -> {
+            authenticateWithRetry(Optional.of(3), 5000, Promise.promise(), authentication -> {
+                if (authentication.succeeded()) {
+                    final User newToken = authentication.result();
 
-                return shareAccessToken(newToken);
-            }).onFailure(details -> LOGGER.error(details.getMessage()));
+                    myTimerId = keepTokenFresh(newToken, aSecondsBeforeExpiration);
+
+                    shareAccessToken(newToken);
+                } else {
+                    LOGGER.error(authentication.cause().getMessage());
+                }
+            });
+        });
+    }
+
+    /**
+     * Attempts authentication with a set number of retries. This method is recursive.
+     *
+     * @param aRetryCount The optional number of times to retry (retries forever if empty)
+     * @param aRetryDelay The time to wait between retry attempts, in milliseconds
+     * @param aPromise The promise that will be completed if authentication succeeds, or failed otherwise
+     * @param aResultHandler The handler that will be called with the result of {@code aPromise}
+     */
+    private void authenticateWithRetry(final Optional<Integer> aRetryCount, final long aRetryDelay,
+            final Promise<User> aPromise, final Handler<AsyncResult<User>> aResultHandler) {
+        myAuthProvider.authenticate(new JsonObject()).onFailure(failure -> {
+            if (aRetryCount.isEmpty() || aRetryCount.get() > 0) {
+                // Wait a bit before retrying again
+                myVertx.executeBlocking(promise -> {
+                    try {
+                        Thread.sleep(aRetryDelay);
+                        promise.complete();
+                    } catch (final InterruptedException details) {
+                        promise.fail(details);
+                    }
+                }, result -> {
+                    authenticateWithRetry(aRetryCount.map(count -> count - 1), aRetryDelay, aPromise, aResultHandler);
+                });
+            } else {
+                aPromise.fail(failure);
+            }
         });
     }
 }
